1a3af03b42bb9c7472b361ed325f07a55bc954c5
Kislay Kanti Dhar <kislayof@gmail.com>
2021-10-13 00:03:44 +0530

    comments and posts are made and tested

---
+++ comments/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostsId =  {};
+
+app.get('/posts/:id/comments',(req,res)=>{
+    res.send(commentsByPostsId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments',(req,res)=>{
+    const commentId = randomBytes(4).toString('hex');
+    console.log(`comment created =>${commentId}`);
+    const comments = commentsByPostsId[req.params.id] || [] ; 
+    const {content} = req.body;
+    comments.push({
+        id: commentId,
+        content
+    });
+    commentsByPostsId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001,()=>{
+    console.log('posts server running at 4001');
+});
