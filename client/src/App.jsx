785ce679ac74fe7f49c09362f5003a6426a15711
Kislay Kanti Dhar <kislayof@gmail.com>
2021-10-13 21:20:55 +0530

    basic app is complete,up next:implementation of messaging service

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PostCreate from './PostCreate';
+import PostsList from './PostsList';
+
+const app = () => {
+    return(
+        <div className="container">
+            <h1>
+                Create Post
+            </h1>
+            <PostCreate />
+            <hr />
+            <h1>
+                Posts
+            </h1>
+            <PostsList />
+        </div>
+    )
+}
+
+export default app;
